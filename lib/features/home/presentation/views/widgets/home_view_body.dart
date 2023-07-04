9394781d7e80e17d201861c06e7ba7b50cca7562
Mahmoud <mahmoud1528346@gmail.com>
2023-07-04 22:07:43 +0300

    Best Seleer List ViewI tem part One

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:bookly/core/utils/styles.dart';
 import 'package:flutter/material.dart';
+import '../../../../../core/utils/assets.dart';
 import 'custome_appbar.dart';
 import 'featured_list_view.dart';
 
@@ -16,11 +17,44 @@ class HomeViewBody extends StatelessWidget {
           CustomeAppBAr(),
           FeaturedBookListView(),
           SizedBox(height: 50),
-          Text("Best Seller", style: Styles.titleMudium,)
+          Text("Best Seller", style: Styles.titleMudium,),
+          SizedBox(height: 50),
+          BestSeleerListViewItem()
         ],
       ),
     );
   }
 }
 
+
+class BestSeleerListViewItem extends StatelessWidget {
+  const BestSeleerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 125,
+      child: Row(
+        children: [
+          AspectRatio(
+        aspectRatio: 2.5 / 4,
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8),
+            color: Colors.green,
+            image: const DecorationImage(
+              fit: BoxFit.fill,
+              image: AssetImage(AssetsData.testImage),
+            ),
+          ),
+        ),
+      ),
+          Column(
+            children: [],
+          ),
+        ],
+      ),
+    );
+  }
+}
 
