eb377badf0a82b4cf28af151ba95f1739302b458
Mahmoud <mahmoud1528346@gmail.com>
2023-07-06 21:43:23 +0300

    best saler item done

--- lib/features/home/presentation/views/widgets/best_saler_List_view_item.dart
+++ lib/features/home/presentation/views/widgets/best_saler_List_view_item.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import '../../../../../constants.dart';
 import '../../../../../core/utils/assets.dart';
 import '../../../../../core/utils/styles.dart';
+import 'booking_ratting.dart';
 
 class BestSeleerListViewItem extends StatelessWidget {
   const BestSeleerListViewItem({super.key});
@@ -25,36 +27,43 @@ class BestSeleerListViewItem extends StatelessWidget {
             ),
           ),
           const SizedBox(width: 30),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                width: MediaQuery.of(context).size.width * 0.5,
-                child: Text(
-                  "Harry Potter And the Gobelt of Fire",
-                  style: Styles.textStyle20.copyWith(fontFamily: kGtSectraFine),
-                  maxLines: 2,
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.5,
+                  child: Text(
+                    "Harry Potter And the Gobelt of Fire",
+                    style:
+                        Styles.textStyle20.copyWith(fontFamily: kGtSectraFine),
+                    maxLines: 2,
+                  ),
                 ),
-              ),
-              const SizedBox(height: 3),
-              const Text(
-                "J.K Rowling",
-                style: Styles.textStyle14,
-              ),
-              const SizedBox(height: 3),
-              Row(
-                children: [
-                  Text(
-                    "19.99 EUR",
-                    style: Styles.textStyle20
-                        .copyWith(fontWeight: FontWeight.bold),
-                  ),                 
-                ],
-              ),
-            ],
+                const SizedBox(height: 3),
+                const Text(
+                  "J.K Rowling",
+                  style: Styles.textStyle14,
+                ),
+                const SizedBox(height: 3),
+                Row(
+                  children: [
+                    Text(
+                      "19.99 €",
+                      style: Styles.textStyle20
+                          .copyWith(fontWeight: FontWeight.bold),
+                    ),
+                    const Spacer(),
+                    const BookRating()
+                  ],
+                ),
+              ],
+            ),
           ),
         ],
       ),
     );
   }
 }
+
+
