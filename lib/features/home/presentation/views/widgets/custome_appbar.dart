2d597b7814365c6f23669ef75dc22c00ba8c8ffc
Mahmoud <mahmoud1528346@gmail.com>
2023-07-21 00:46:03 +0300

    create coustome search text field

--- lib/features/home/presentation/views/widgets/custome_appbar.dart
+++ lib/features/home/presentation/views/widgets/custome_appbar.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:go_router/go_router.dart';
-
 import '../../../../../core/utils/app_router.dart';
 import '../../../../../core/utils/assets.dart';
 
@@ -10,16 +9,24 @@ class CustomeAppBAr extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Padding(
+    return Padding(
       padding: const EdgeInsets.only(top: 40, bottom: 20),
       child: Row(
         children: [
-          Image.asset(AssetsData.logo, height: 25,), 
+          Image.asset(
+            AssetsData.logo,
+            height: 25,
+          ),
           const Spacer(),
-          IconButton(onPressed: (){
-            GoRouter.of(context).push(AppRouter.kSearchViewBody);
-            
-          }, icon: const Icon(FontAwesomeIcons.magnifyingGlass, size: 24,))
+          IconButton(
+            onPressed: () {
+              GoRouter.of(context).push(AppRouter.kSearchView);
+            },
+            icon: const Icon(
+              FontAwesomeIcons.magnifyingGlass,
+              size: 22,
+            ),
+          )
         ],
       ),
     );
--- lib/features/search/presentation/views/widgets/search_view_body.dart
+++ lib/features/search/presentation/views/widgets/search_view_body.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'custome_search_text_field.dart';
 
 class SearchViewBody extends StatelessWidget {
   const SearchViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("data"));
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 30),
+      child: Column(
+        children: [
+          CustomeSearchTextField(),
+        ],
+      ),
+    );
   }
 }
