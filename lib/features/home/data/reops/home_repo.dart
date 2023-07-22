818b3b0c7cf8ebcded7783b6000a69172c01a61f
Mahmoud <mahmoud1528346@gmail.com>
2023-07-22 16:30:43 +0300

    implemnts fetch newset books

--- lib/features/home/data/reops/home_repo.dart
+++ lib/features/home/data/reops/home_repo.dart
@@ -3,6 +3,6 @@ import '../models/book_model/book_model.dart';
 import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo {
-  Future<Either<Failuer, List<BookModel>>> fetchBestSellerBooks();
+  Future<Either<Failuer, List<BookModel>>> fetchNewsetBooks();
   Future<Either<Failuer, List<BookModel>>> fetchFeaturedBooks();
 }
